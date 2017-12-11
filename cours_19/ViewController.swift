8164aed31694d800b616ae57afb6def462647b7f
eleves <you@yourdomain.com>
2017-12-11 11:10:20 -0500

    version 1

--- cours_19/ViewController.swift
+++ cours_19/ViewController.swift
@@ -1,25 +1,57 @@
-//
-//  ViewController.swift
-//  cours_19
-//
-//  Created by eleves on 2017-12-05.
-//  Copyright © 2017 eleves. All rights reserved.
-//
-
+//==================
 import UIKit
-
+//==================
 class ViewController: UIViewController {
-
+    //--------------------
+    @IBOutlet weak var wheel: UIImageView!
+    @IBOutlet weak var ball: UIView!
+    @IBOutlet weak var target: UIView!
+    @IBOutlet weak var kobolt: UIImageView!
+    //--------------------
+    var aTimer: Timer!
+    var cos: Double!
+    var sin: Double!
+    //--------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ball.layer.cornerRadius = 12.5
+        target.layer.cornerRadius = 12.5
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //--------------------
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            kobolt.image = UIImage(named: "kobolt.png")
+            if touch.view == self.view {
+                if aTimer != nil {
+                    aTimer.invalidate()
+                    aTimer = nil
+                }
+                ball.center.x = UIScreen.main.bounds.width / 2
+                ball.center.y = UIScreen.main.bounds.height - 260
+                target.center.x = touch.location(in: self.view).x
+                target.center.y = touch.location(in: self.view).y
+                let opp = touch.location(in: self.view).y - (UIScreen.main.bounds.height - 260)
+                let adj = touch.location(in: self.view).x - (UIScreen.main.bounds.width / 2)
+                let radians = atan2f(Float(opp),Float(adj))
+                let degrees = radians * 180 / Float(Double.pi)
+                cos = __cospi(Double(degrees/180))
+                sin = __sinpi(Double(degrees/180))
+                aTimer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(animate), userInfo: nil, repeats: true)
+            }
+        }
     }
-
-
+    //--------------------
+    @objc func animate() {
+        if ball.frame.intersects(target.frame) {
+            kobolt.image = UIImage(named: "KoboldDie.png")
+            ball.center.x = target.center.x
+            ball.center.y = target.center.y
+            aTimer.invalidate()
+            aTimer = nil
+        }
+        ball.center.x += CGFloat(cos)
+        ball.center.y += CGFloat(sin)
+    }
+    //--------------------
 }
-
+//==================
