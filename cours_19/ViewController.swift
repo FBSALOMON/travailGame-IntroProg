c9ab9d6e219b55e0c75d17d6f17b50ed338cdd8a
eleves <you@yourdomain.com>
2017-12-12 18:21:13 -0500

    Version (Change Monsters)

--- cours_19/ViewController.swift
+++ cours_19/ViewController.swift
@@ -5,9 +5,10 @@ class ViewController: UIViewController {
     //--------------------
     @IBOutlet weak var BackGround: UIImageView!
     @IBOutlet weak var ball: UIView!
-    @IBOutlet weak var enemyAttack: UIView!
+    @IBOutlet weak var enemyAttackView: UIView!
+    @IBOutlet weak var enemyAttackImage: UIImageView!
     @IBOutlet weak var target: UIView!
-    @IBOutlet weak var kobold: UIImageView!
+    @IBOutlet weak var monsterImage: UIImageView!
     @IBOutlet weak var Elminster: UIImageView!
     @IBOutlet weak var Player: UIView!
     @IBOutlet weak var expAtual: UIView!
@@ -27,12 +28,18 @@ class ViewController: UIViewController {
     let loop = true
     var numberOfAttacks = 0
     var level = 1
+    var monsterName: String!
+    var monsterSpell: String!
+    var monsterImageDead: String!
     //--------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         ball.layer.cornerRadius = 12.5
         target.layer.cornerRadius = 12.5
-        enemyAttack.layer.cornerRadius = 12.5
+        enemyAttackView.layer.cornerRadius = 12.5
+        monsterName = "lightElemental.png"
+        monsterSpell = "lightning.png"
+        monsterImageDead = "lightElementalDead.png"
         enemyAttackAnimation()
         
     }
@@ -71,7 +78,6 @@ class ViewController: UIViewController {
     }
     //--------------------
     @objc func animate() {
-
         if distance > 1250 {
             aTimer.invalidate()
             aTimer = nil
@@ -83,20 +89,19 @@ class ViewController: UIViewController {
                 aTimerEnemy.invalidate()
                 aTimerEnemy = nil
             }
-            kobold.image = UIImage(named: "lightElementalDead.png")
+            self.monsterImage.image = UIImage(named: self.monsterImageDead)
             ball.center.x = -150
             ball.center.y = -150
             aTimer.invalidate()
             aTimer = nil
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
                 self.experiencia()
-                self.kobold.image = UIImage(named: "lightElemental.png")
                 self.target.center.x = CGFloat(arc4random_uniform(UInt32(UIScreen.main.bounds.width - 181)) + 100)
                 self.target.center.y = (CGFloat(arc4random_uniform(UInt32(UIScreen.main.bounds.width - 500)) + 85))
                 self.distance = 0
                 self.numberOfAttacks = 0
-                self.enemyAttack.center.x = self.target.center.x
-                self.enemyAttack.center.y = self.target.center.y
+                self.enemyAttackView.center.x = self.target.center.x
+                self.enemyAttackView.center.y = self.target.center.y
                 self.enemyAttackAnimation()
             })
         }
@@ -130,14 +135,14 @@ class ViewController: UIViewController {
         let adj = CGFloat(arc4random_uniform(500)) + (target.center.x - (Player.center.x)) - CGFloat(arc4random_uniform(500))
         let radians = atan2f(Float(opp),Float(adj))
         let degrees = radians * 180 / Float(Double.pi)
-        enemyAttack.center.x = target.center.x
-        enemyAttack.center.y = target.center.y
+        enemyAttackView.center.x = target.center.x
+        enemyAttackView.center.y = target.center.y
         cosEnemy = __cospi(Double(degrees/180))
         sinEnemy = __sinpi(Double(degrees/180))
     }
     
     @objc func animateEnemy() {
-        
+        changeMonsters()
         if distanceEnemy > 1250 {
             distanceEnemy = 0
             numberOfAttacks += 1
@@ -145,15 +150,15 @@ class ViewController: UIViewController {
             changeMonsterAttackDirection()
             return
         }
-        if enemyAttack.frame.intersects(Player.frame) {
+        if enemyAttackView.frame.intersects(Player.frame) {
             takeDamage()
             changeMonsterAttackDirection()
-            enemyAttack.center.x = target.center.x
-            enemyAttack.center.y = target.center.y
+            enemyAttackView.center.x = target.center.x
+            enemyAttackView.center.y = target.center.y
             distanceEnemy = 0
         }
-        enemyAttack.center.x -= CGFloat(cosEnemy)
-        enemyAttack.center.y -= CGFloat(sinEnemy)
+        enemyAttackView.center.x -= CGFloat(cosEnemy)
+        enemyAttackView.center.y -= CGFloat(sinEnemy)
         distanceEnemy += 1
     }
     
@@ -168,6 +173,7 @@ class ViewController: UIViewController {
             fireBall.frame.size.height += 15
             level += 1
             levelLabel.text = "LEVEL: " + String(level)
+            changeMonsters()
         }
     }
     
@@ -179,6 +185,28 @@ class ViewController: UIViewController {
             aTimerEnemy.invalidate()
             aTimerEnemy = nil
         }
+    }
+    
+    func changeMonsters() {
+        if (level == 1) {
+            monsterImage.image = UIImage(named: "kobolt.png")
+            enemyAttackImage.image = UIImage (named: "lightning.png")
+            monsterName = "kobolt.png"
+            monsterSpell = "lightning.png"
+            monsterImageDead = "koboltDie.png"
+        } else if (level == 2) {
+            monsterImage.image = UIImage(named: "lightElemental.png")
+            enemyAttackImage.image = UIImage (named: "lightning.png")
+            monsterName = "lightElemental.png"
+            monsterSpell = "fireball.png"
+            monsterImageDead = "lightElementalDead.png"
+            target.frame.size.width = 150
+            target.frame.size.width = 188
+            monsterImage.frame.size.width = 150
+            monsterImage.frame.size.height = 188
+        }
+        monsterImage.image = UIImage(named: monsterName)
+        enemyAttackImage.image = UIImage(named: monsterSpell)
     }
 }
 //==================
